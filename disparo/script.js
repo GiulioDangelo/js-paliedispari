95cc2655526c018899705b9db1dad7ed5af08ede
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-20 22:15:27 +0200

    function fix

--- disparo/script.js
+++ disparo/script.js
@@ -1,39 +1,43 @@
-let choice = prompt("scegli tra pari e dispari")
+let choice = prompt("scegli tra pari e dispari").toLowerCase();
 let num = parseInt(prompt("inserisci un numero tra 1 e 5"));
-let cpuNum = randomNumber(1,5)
-let finalNum = num + cpuNum
+let cpuNum = randomNumber(1, 5);
+let finalNum = num + cpuNum;
 
-let i=0
-while (choice !== "pari"){
-    let choice2 = prompt("scegli tra pari e dispari")
-}
+
+// while (choice !== "pari" || choice !== "dispari") {
+// 	choice = prompt("scegli tra pari e dispari");
+// }
+
+
+// while (isNaN(num) || num < 1 || num > 5) {
+// 	num = parseInt(prompt('inserisci un valore corretto'));
+// }
+
 
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let flag = "pari"
+
+let flag = "pari";
 function isOdd(number) {
-    if (finalNum % 2 === 1) {
-        return flag = "dispari"
-    }
-    else{
-        return flag = "pari"
-    }
+	if (number % 2 === 1) {
+		return (flag = "dispari");
+	} else {
+		return (flag = "pari");
+	}
 }
 
 
 if (choice == "pari" && isOdd(finalNum) == "pari") {
-    console.log("hai vinto e' pari")
-}
-else if(choice == "dispari" && isOdd(finalNum) == "dispari"){
-    console.log("hai vinto e' dispari")
-}
-else if(choice == "dispari" && isOdd(finalNum) == "pari"){
-    console.log("hai perso e' pari")
-}
-else if(choice == "pari" && isOdd(finalNum) == "dispari"){
-    console.log("hai perso e' dispari")
+	console.log("hai vinto e' pari");
+} else if (choice == "dispari" && isOdd(finalNum) == "dispari") {
+	console.log("hai vinto e' dispari");
+} else if (choice == "dispari" && isOdd(finalNum) == "pari") {
+	console.log("hai perso e' pari");
+} else if (choice == "pari" && isOdd(finalNum) == "dispari") {
+	console.log("hai perso e' dispari");
 }
 
-console.log(finalNum)
+
+console.log(finalNum);
