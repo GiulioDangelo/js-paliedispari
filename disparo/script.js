fb3e2e3dc07edc7da6432f624cc186d2e65bf05c
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-20 23:09:29 +0200

    fix while loop and add comment

--- disparo/script.js
+++ disparo/script.js
@@ -1,24 +1,34 @@
 let choice = prompt("scegli tra pari e dispari").toLowerCase();
+
+//check if user inserted "pari" or "dispari"
+while (choice != "pari" && choice != "dispari"){
+   alert('inserisci un valore corretto')
+	choice = prompt("scegli tra pari e dispari");
+}
+
+
 let num = parseInt(prompt("inserisci un numero tra 1 e 5"));
-let cpuNum = randomNumber(1, 5);
-let finalNum = num + cpuNum;
+
+//check if user inserted a number between 1 and 5
+while (isNaN(num) || num < 1 || num > 5) {
+   alert('inserisci un valore corretto')
+	num = parseInt(prompt('inserisci un numero tra 1 e 5'));
+}
 
 
-// while (choice !== "pari" || choice !== "dispari") {
-// 	choice = prompt("scegli tra pari e dispari");
-// }
-
-
-// while (isNaN(num) || num < 1 || num > 5) {
-// 	num = parseInt(prompt('inserisci un valore corretto'));
-// }
-
-
+//get a random integer between a min and a max value
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
+//generate a random number between 1 and 5 and add it to the one picked by the user
+let cpuNum = randomNumber(1, 5);
+let finalNum = num + cpuNum;
+
+
+
+//check if the value inserted and the one generated are even or odd
 let flag = "pari";
 function isOdd(number) {
 	if (number % 2 === 1) {
@@ -29,6 +39,7 @@ function isOdd(number) {
 }
 
 
+//check if the user won
 if (choice == "pari" && isOdd(finalNum) == "pari") {
 	console.log("hai vinto e' pari");
 } else if (choice == "dispari" && isOdd(finalNum) == "dispari") {
